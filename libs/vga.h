dc1869e92bd54f75bf7c08bea7099f8fd0444dfc
Loco Mia <stikmenamerica@gmail.com>
2025-12-07 06:16:14 -0500

    updated vga.h

--- libs/vga.h
+++ libs/vga.h
@@ -3,8 +3,11 @@
 #define VIDMEM ((char*)0xB8000)
 
 extern unsigned int cursor;
+extern unsigned int cursor_pos;
+extern unsigned int blink_timer;
 
 void printstr(const char *str);
 void putchar(char c);
 void clearscreen(void);
-void newline();
+void newline(void);
+void update_cursor(void);
